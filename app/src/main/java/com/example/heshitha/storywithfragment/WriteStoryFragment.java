772dd766dfd1d52a15847c57916c4bdae746a948
Heshitha Chathuranga <heshithachathuranga@gmail.com>
2015-03-13 16:32:02 +0530

    @

---
+++ app/src/main/java/com/example/heshitha/storywithfragment/WriteStoryFragment.java
@@ -0,0 +1,30 @@
+package com.example.heshitha.storywithfragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WriteStoryFragment extends Fragment {
+
+
+    public WriteStoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_write_story, container, false);
+    }
+
+
+}
